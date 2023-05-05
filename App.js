cb5756db83c496a51a7ebebc6a22bfe1060122fc
Oded <odedatias8115@gmail.com>
2023-05-05 13:04:43 +0300

    Adding the project fonts

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+// Hooks imports
+import { useFonts } from 'expo-font';
+
 // RN core components & API imports
 import { StatusBar } from 'expo-status-bar';
 
+// Fonts imports
+import Anton from './assets/fonts/Anton-Regular.ttf';
+import MerriWeatherSans from './assets/fonts/MerriweatherSans-VariableFont_wght.ttf';
+import Righteous from './assets/fonts/Righteous-Regular.ttf';
+
 // App component
 export default function App() {
-	return <></>;
+	// Loading the fonts
+	const [loaded] = useFonts({
+		anton: Anton,
+		merriWeatherSans: MerriWeatherSans,
+		righteous: Righteous,
+	});
+
+	if (loaded) return <></>;
 }
