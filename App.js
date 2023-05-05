609c9e3a211446255507a85c2fba537685557e2f
Oded <odedatias8115@gmail.com>
2023-05-05 13:01:36 +0300

    Project Cleanup

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
+// RN core components & API imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+// App component
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return <></>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
