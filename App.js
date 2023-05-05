8054606d5b9c8cb1541a8e4b589070ca4b26861c
Oded <odedatias8115@gmail.com>
2023-05-05 13:19:53 +0300

    Adding The main Stack navigator of the project

--- App.js
+++ App.js
@@ -9,6 +9,17 @@ import Anton from './assets/fonts/Anton-Regular.ttf';
 import MerriWeatherSans from './assets/fonts/MerriweatherSans-VariableFont_wght.ttf';
 import Righteous from './assets/fonts/Righteous-Regular.ttf';
 
+// Custom components imports
+import Login from './screens/Login';
+import BottomTab from './components/navigators/BottomTab';
+
+// Navigation Imports
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Navigators
+const Stack = createNativeStackNavigator();
+
 // App component
 export default function App() {
 	// Loading the fonts
@@ -22,6 +33,18 @@ export default function App() {
 		return (
 			<>
 				<StatusBar style='dark' />
+				<NavigationContainer>
+					<Stack.Navigator
+						screenOptions={{
+							headerShown: false,
+							contentStyle: {
+								backgroundColor: COLORS.background,
+							},
+						}}>
+						<Stack.Screen name='Login' component={Login} />
+						<Stack.Screen name='BottomTabs' component={BottomTab} />
+					</Stack.Navigator>
+				</NavigationContainer>
 			</>
 		);
 }
---
+++ components/navigators/BottomTab.js
@@ -0,0 +1,16 @@
+// RN core components & API imports
+import { StyleSheet, Text, View } from 'react-native';
+
+// BottomTab componet
+const BottomTab = () => {
+	return (
+		<View>
+			<Text>BottomTab</Text>
+		</View>
+	);
+};
+
+export default BottomTab;
+
+// BottomTab StyleSheet
+const styles = StyleSheet.create({});
