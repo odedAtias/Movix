27203576b01d12d5b1e2cf2064f2253f631b1646
Oded <odedatias8115@gmail.com>
2023-05-05 13:25:05 +0300

    Adding the project colors

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const COLORS = {
+	primary: '#BB86FC',
+	primaryVariant: '#0073CF',
+	secondary: '#03DAC6',
+	background: '#121212',
+	error: '#CF6679',
+	onBackground1: '#FFF',
+	onBackground2: '#a9a9a9',
+	facebook: '#4267b2',
+	google: '#ed0707',
+};
